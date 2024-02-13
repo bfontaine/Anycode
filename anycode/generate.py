eb21fc8d0af7cb8a774b1f17bd5494f0251ca59f
Baptiste Fontaine <b@ptistefontaine.fr>
2024-02-13 22:59:52 +0100

    generate_any: fix constant detection

--- anycode/generate.py
+++ anycode/generate.py
@@ -37,38 +37,94 @@ class GenerationException(Exception):
         self.openai_response = openai_response
 
 
-def complete(*, init_messages: list[dict], message: str, model="gpt-3.5-turbo") -> str:
+def complete(*, init_messages: List[dict], message: str, model="gpt-3.5-turbo") -> str:
     response = openai.chat.completions.create(
         model=model,
-        messages=init_messages + [
-            {"role": "user", "content": message}
-        ],
+        messages=cast(
+            # https://github.com/openai/openai-python/issues/911#issuecomment-1834547461
+            List[ChatCompletionMessageParam],
+            init_messages + [
+                {"role": "user", "content": message},
+            ]
+        ),
     )
 
-    return response.choices[0].message.content
+    return cast(str, response.choices[0].message.content)
 
 
-def generate_constant(name: str) -> str | int | float | bool | list | dict[str, Any] | None:
+def generate_constant(name: str) -> str | int | float | bool | list | Dict[str, Any] | None:
+    message = f"{name} = "
     openai_response = complete(
         init_messages=CONSTANT_INIT_MESSAGES,
-        message=f"{name} = ",
+        message=message,
     )
 
     try:
         return json.loads(openai_response)
     except JSONDecodeError as e:
         raise GenerationException(
-            query=name,
+            query=message,
             openai_response=openai_response,
         ) from e
 
 
+def _generate_args_string(args, kwargs) -> str:
+    args_string_fragments: List[str] = []
+
+    arg_names = [c for c in string.ascii_lowercase if c not in kwargs]
+    arg_name_index = 0
+
+    for _ in args:
+        arg_name = arg_names[arg_name_index]
+        arg_name_index += 1
+
+        args_string_fragments.append(arg_name)
+
+    args_string_fragments.extend(kwargs)
+
+    return ", ".join(args_string_fragments)
+
+
 def generate_function(name: str) -> Callable:
-    pass  # TODO
+    def fn(*args, **kwargs):
+        if not hasattr(fn, FUNCTION_ATTR_NAME):
+            args_string = _generate_args_string(args, kwargs)
+            function_signature = f"def {name}({args_string}):"
+
+            openai_response = complete(
+                init_messages=FUNCTION_INIT_MESSAGES,
+                message=function_signature,
+            )
+
+            try:
+                code = compile(openai_response, "<string>", mode="exec")
+            except SyntaxError as e:
+                raise GenerationException(
+                    function_signature,
+                    openai_response=openai_response,
+                ) from e
+
+            scope = dict(inspect.stack()[0].frame.f_globals)
+            exec(code, scope)  # YOLO
+
+            if name in scope:
+                openai_fn = scope[name]
+                setattr(fn, FUNCTION_ATTR_NAME, openai_fn)
+                setattr(fn, "_openai_response", openai_response)
+            else:
+                raise GenerationException(
+                    function_signature,
+                    openai_response=openai_response,
+                )
+
+        return getattr(fn, FUNCTION_ATTR_NAME)(*args, **kwargs)
+
+    fn.__name__ = name
+    return fn
 
 
 def generate_any(name: str):
-    if name.upper():
+    if name.isupper():
         return generate_constant(name)
 
     return generate_function(name)
